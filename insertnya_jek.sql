2d2c3d341f41151841c1c983b7dce9bf791e4d0b
coll-j <zumazaki@gmail.com>
2021-06-29 19:39:22 +0700

    insertnya jek

--- insertnya_jek.sql
+++ insertnya_jek.sql
@@ -33,11 +33,30 @@ insert into kep (kep_nama, kep_email, kep_password) values ('test', 'tets', 'tes
 delete from kep
 
 -- Generate records
-insert into kep (kep_nama, kep_email, kep_password, kep_reknamabank, kep_statusaktif, kep_statusdate)
-select substr(md5(random()::text), 0, 25), -- nama
-concat(substr(md5(random()::text), 0, 5), '@mail.com'), -- email
-substr(md5(random()::text), 0, 25), -- pass
+-- kep
+insert into kep (kep_id, kep_nama, kep_reknamabank, kep_statusaktif, kep_statusdate)
+select generate_series(1, 10000) kep_id, -- id
+substr(md5(random()::text), 0, 25), -- nama
 (array['Mandiri', 'BCA', 'BNI'])[floor(random() * 3 + 1)], -- nama bank
 round(random())::int::bool, -- kep_statusaktif
 timestamp '2018-01-10 20:00:00' + random() * (timestamp '2020-12-31 20:00:00' - timestamp '2018-01-10 10:00:00') -- kep_statusdate
-from generate_series(1, 1000);
+;
+
+-- sk kep
+insert into sk_kep (sk_id, kep_id, sk_validstart, sk_validend)
+select kep_id as sk_id, kep_id, kep_statusdate as sk_validstart, kep_statusdate + 90 as sk_validend -- 3 bulan
+from kep
+
+-- daftar harga layanan
+insert into daftar_harga_layanan (dhl_id, kep_id, dhl_nama, dhl_biaya)
+select generate_series(1, 100000) 
+,trunc(random() * 10000 + 1)
+,(array['Penelitian Doktor', 'Penelitian Mahasiswa', 'Penelitian Kesehatan', 'Swasta', 'BUMN'])[floor(random() * 5 + 1)]
+,trunc(random() * 1000 + 100) * 1000
+
+-- anggota
+insert into anggota (ag_id, ag_nama, ag_institusiasal, jenis_kelamin)
+select generate_series(1, 100000)
+,concat((array['Anggun ', 'Bayu ', 'Cece ', 'Dirga ', 'Ehehe '])[floor(random() * 5 + 1)], substr(md5(random()::text), 0, 5))-- nama
+,(array['ITS', 'UI', 'Unair', 'Telkom', 'UPNVJ'])[floor(random() * 5 + 1)]
+,(array['P', 'L', 'N'])[floor(random() * 3 + 1)]
